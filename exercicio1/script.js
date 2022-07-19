50dfe4019e8f1af86c06ff645bd325a8bc654ca6
Lorenzo <lorenzo.baridotti@hotmail.com>
2022-07-19 20:47:02 -0300

    Exercícios para praticar A007

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -9,8 +9,9 @@ console.log (typeof nome, typeof idade)
 
 nome = prompt ('qual seu nome?')
 idade = +prompt ("quantos anos vc tem?")
+const anoDeNascimento = 2022 - Number(idade)
 
 //prompt irá atribuir string como valor e +prompt ira atribuir number como valor
 
 console.log (typeof nome, typeof idade)
-console.log("Olá,", nome, "você tem", idade, "anos")
+console.log("Olá,", nome, "você tem", idade, "anos, e nasceu no ano de", anoDeNascimento)
