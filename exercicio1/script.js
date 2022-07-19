9ca1bcbfd6c237dcb7aa983d7f454f086dff9658
Lorenzo <lorenzo.baridotti@hotmail.com>
2022-07-19 19:58:15 -0300

    Exercícios para praticar A007

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,7 +3,7 @@ let idade
 
 console.log (typeof nome, typeof idade)
 
-//undefined foi impresso pois nãoa tribui valor as variáveis
+//undefined foi impresso pois não atribui valor as variáveis
 
 nome = prompt ('qual seu nome?')
 idade = +prompt ("quantos anos vc tem?")
