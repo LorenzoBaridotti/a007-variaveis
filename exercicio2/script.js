ef94b5723cd651af535bbc8c01a8f7c703c636bd
Lorenzo <lorenzo.baridotti@hotmail.com>
2022-07-19 20:19:31 -0300

    Exercícios para praticar A007

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -8,12 +8,6 @@ const comida = prompt(perguntaC)
 
 //Variavel jogo aquivale à variavel perguntaA, assim como time equivale à perguntaB e comida à perguntaC
 
-let CSGO = jogo
-let Athletico = time
-let milho = comida
-
-//Com isso CSGO será a resposta atribuida à perguntaA, Atlhetico será a resposta atribuida à perguntaB e milho será a resposta atribuida à perguntaC
-
-console.log('Você joga CSGO?', CSGO)
-console.log('Você torce para o Athletico?', Athletico)
-console.log('Você gosta de milho?', milho)
+console.log('Você joga CSGO?', jogo)
+console.log('Você torce para o Athletico?', time)
+console.log('Você gosta de milho?', comida)
